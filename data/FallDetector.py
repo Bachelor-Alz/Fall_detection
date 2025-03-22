0bafd17f1cb0b961067e006d29f4f5ceb09feee7
Andreas Mertz <andreasmertz11@gmail.com>
2025-03-22 18:14:05 +0100

    Refined UMA_fall_timestamps and fixed bug which caused 60% of the dataset not to be loaded, specifically ADL's

--- data/FallDetector.py
+++ data/FallDetector.py
@@ -53,7 +53,8 @@ class FallDetector:
 
         columns_to_scale = ['accel_x_list', 'accel_y_list', 'accel_z_list',
                             'gyro_x_list', 'gyro_y_list', 'gyro_z_list']
-        
+
+
         for col in columns_to_scale:
             uma_df.loc[:, col] = uma_df[col].apply(self.fix_multiple_periods)
             weda_df.loc[:, col] = weda_df[col].apply(self.fix_multiple_periods)
@@ -72,7 +73,6 @@ class FallDetector:
         up_df.loc[:, columns_to_scale] = scaler_up.fit_transform(up_df[columns_to_scale])
         
         df = pd.concat([uma_df, weda_df, up_df])
-
         grouped = df.groupby('filename')
         resampled_df = []
 
@@ -80,45 +80,37 @@ class FallDetector:
             group_resampled = self._resample_data(group)
             resampled_df.append(group_resampled)
 
-
-        
-
         df_resampled = pd.concat(resampled_df)
-        df_resampled.dropna(inplace=True)
         scalar = StandardScaler()
 
         df_resampled.loc[:, columns_to_scale] = scalar.fit_transform(df_resampled[columns_to_scale])
         print("Pre-processed data")
         return df_resampled
-    
+   
 
     def _resample_data(self, group: pd.DataFrame):
         """Resample the data to 50Hz (20ms interval) while handling duplicates and non-numeric columns.
         Also aligns the time to start at 00:00.000.
         """  
-        group = group.drop_duplicates(subset=['time'])
         group.set_index('time', inplace=True)
-
-        # Snap timestamps to the nearest 20ms
-        group.index = group.index.round('20ms')
-        
-        # Drop duplicate timestamps
+        ms = '20.00ms'
+        group.index = group.index.round(ms)
         group = group[~group.index.duplicated(keep='first')]
-
         # Drop the non-numeric column 'filename'
         numeric_group = group.drop(columns=['filename'])
-         # Align start time to nearest 20ms
-        new_start_time = numeric_group.index.min().floor('20ms')  
+
+        # Align start time to nearest 20ms
+        new_start_time = numeric_group.index.min().floor(ms)  
         new_time_index = pd.date_range(start=new_start_time, 
                                     end=numeric_group.index.max(), 
-                                   freq='20ms')
+                                    freq=ms)
         
-        numeric_resampled = numeric_group.reindex(new_time_index)
-        interpolated = numeric_resampled.interpolate(method='linear').bfill().ffill()
-        interpolated.reset_index(inplace=True)
-        interpolated.rename(columns={'index': 'time'}, inplace=True)
-        interpolated['filename'] = group['filename'].iloc[0]
-        return interpolated
+        numeric_resampled = numeric_group.reindex(new_time_index).interpolate(method='linear').bfill().ffill()
+        numeric_resampled.reset_index(inplace=True)
+        numeric_resampled.rename(columns={'index': 'time'}, inplace=True)
+        numeric_resampled['filename'] = group['filename'].iloc[0]
+        return numeric_resampled
+
 
 
     def extract_features(self, df: pd.DataFrame):
@@ -133,8 +125,7 @@ class FallDetector:
             for filename, group in grouped:
                 for i in range(0, len(group) - window_size + 1, step_size):
                     window = group.iloc[i:i + window_size]
-                    if not (window['start_time'].isna().any() or window['end_time'].isna().any()):
-                        futures.append(executor.submit(FallDetector.process_window, window, filename, window_size))
+                    futures.append(executor.submit(FallDetector.process_window, window, filename, window_size))
 
             for future in futures:
                 features.append(future.result())
@@ -147,12 +138,17 @@ class FallDetector:
     
     @staticmethod
     def process_window(window, filename, window_size):
+            if(window['start_time'] is None or window['end_time'] is None):
+                is_fall = 0
+            else:
+                is_fall = 1 if ((window['time'] >= window['start_time']) & 
+                                (window['time'] <= window['end_time'])).sum() / window_size >= 0.5 else 0
+            
             feature_dict = {
                 'filename': filename,
                 'start_time': window['time'].iloc[0],
                 'end_time': window['time'].iloc[-1],
-                'is_fall': 1 if ((window['time'] >= window['start_time']) & 
-                                (window['time'] <= window['end_time'])).sum() / window_size >= 0.5 else 0
+                'is_fall': is_fall
             }
 
             columns_to_use = ['gyro_x_list', 'gyro_y_list', 'gyro_z_list', 
